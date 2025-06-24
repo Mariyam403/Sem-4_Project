4a1a414ca3e32c457614fc07368909bda827669f
Mariyam403 <mominmariyam403@gmail.com>
2025-06-24 13:43:10 +0530

    Update Main_pb357.js

--- src/routingpb357/Main_pb357.js
+++ src/routingpb357/Main_pb357.js
@@ -1,8 +1,18 @@
+/* Create react app to perform tasks as asked.
+        First create files as asked below in routing folder
+ 1.        Home.js - for the home page content
+ 2.        Shop.js - for the shop page content
+ 3.        Contact.js - for the contact page content
+ 4.        Nopage.js - for the page other than mentioned links
+ Create Main.js file which contains Links for Home, Shop and Product page. 
+Also, add functionality of page routing.
+ Finally call Main.js in App.js.  */
+
 import { BrowserRouter as Route,Routes,Router,Link } from "react-router-dom";
 import Home from './Home';
 import Contact from './Contact';
 import Shop from './Shop';
 
 const Pb357 = () =>{
-    
+    <
 }
